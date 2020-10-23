3ec104dc8010ce5cba34765390f0e74a1f306c58
JJuanC <camiloflstudio@gmail.com>
2020-10-22 23:58:01 -0500

    Definicion

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,7 +5,29 @@ export default class About extends Component {
     render() {
         return (
             <div className="container-about">
-                <h1 id="title1">Hola desde about</h1>
+                <ul><li><a href="#services">Servicios</a></li></ul>
+                <h1 id="title1">Por que nosotros</h1>
+                   <div className="content-description">
+                   <p id="text-description">
+                     Tenemos los mejores precios del mercado. Nos adaptamos a su presupuesto
+                    <br/>
+                     Fumigación para su casa, finca, negocio, restaurante, escuelas, hospitales, etc
+                    <br/>
+                     Contratos bimestral, trimestral, semestral, anual 
+                    <br/>
+                     Acompañiamiento durante todo el proceso
+                    <br/>
+                    <br/>
+                   En Exprés Fumigaciones realizamos el control de plagas y desinfestacion con productos
+                   piretroides y cipermetrinas de la mejor calidad manejados técnicamente con un grado de
+                   toxicidad |||º teniendo un gran poder de acción y residualidad, no afectan la salud de las
+                   personas o mascotas; no representan daños para los alimentos, medio ambiente o el
+                   mobiliario, son productos inoloros, no visibles y no generan manchas, por lo que no crean
+                   incomodidad en su aplicación, contamos con 5 años de experiencia</p>
+                   <p id="text-description">Nuestro trabajo incluye el certificado para su establecimiento, garantía del mismo, atención
+                    permanente a todas sus necesidades e inquietudes, respectiva capacitación al personal.
+                    </p>
+                   </div>
             </div>
         )
     }
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,6 +5,7 @@ export default class Services extends Component {
     render() {
         return (
             <div className="container-services">
+                <ul><li><a href="#contact">Contacto</a></li></ul>
                 <h1 id="title2">Hola desde Services</h1>
             </div>
         )
